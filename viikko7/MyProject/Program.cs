d80fa949a199a6257acd83463f4fd5214de07d69
caritalin <caritalindf@gmail.com>
2024-02-20 12:58:15 +0200

    viikko7/Program.cs: add

---
+++ viikko7/MyProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Luodaan uusi kategoria
+        Category category1 = new Category("apple", "macos");
+        Category category2 = new Category("microsoft");
+
+        // Luodaan uusi tapahtuma
+        Event event1 = new Event(new DateOnly(2024, 2, 19), "Apple event", category1);
+        Event event2 = new Event(new DateOnly(2024, 2, 20), "Microsoft event", category2);
+
+        // Tulostetaan tapahtumat
+        Console.WriteLine(event1);
+        Console.WriteLine(event2);
+    }
+}
